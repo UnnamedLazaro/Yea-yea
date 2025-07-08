a07646e4bf556d263bd0509f871e7b29ed7d28df
UnnamedLazaro <lazaro.ducanges@escola.pr.gov.br>
2025-07-08 11:59:59 -0300

    gfth

--- Yea-yea-main/perguntas.js
+++ Yea-yea-main/perguntas.js
@@ -1,17 +1,47 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Jogos',
+    'Quem é Toby Fox?',
+    'Robert F. "Toby" Fox (nascido em 11 de outubro de 1991) é um desenvolvedor e compositor de videogames americano. Ele é mais conhecido por desenvolver o RPG Undertale, que foi aclamado e recebeu indicações ao British Academy Game Award , três prêmios Game Awards e um prêmio DICE . Ele também é o criador e chefe do desenvolvimento contínuo de Deltarune, que contém muitos personagens e recursos de Undertale e recebeu elogios significativos.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Jogos',
+    'O que é "AU" nos video-games?',
+    '"AU" significa "Alternative Universe", em português "Universo Alternativo"'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual AU, em Undertale, têm os personagens trocarem de papel?',
+    '"Underswap", Sans tem o objetivo do Papyrus, como Asgore tem o objetivo de Toriel'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 77 + 33?',
+    '110 seu burro'
+)
+
+criaCartao(
+    'Quiz',
+    'O que é isso?',
+    'Uma paroxítona' 
+)
+
+criaCartao(
+    'Quiz',
+    'Quantas evoluções um Eevee, de Pokémon, pode ter?',
+    '8'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Qual linguagem de programação Godot Engine usa?',
+    'GDScript' 
+)
+
+criaCartao(
+    'Jogos',
+    'De onde vem o nome "Sans" de Undertale?',
+    'Comic Sans'
 )
